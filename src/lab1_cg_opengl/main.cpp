233cfe11dcba9a286a048df2c680d3aa1de861df
Zsolt Szilagyi <zsolt.szilagyi@ericsson.com>
2014-09-22 17:00:08 +0200

    format source of lab1

--- src/lab1_cg_opengl/main.cpp
+++ src/lab1_cg_opengl/main.cpp
@@ -6,50 +6,44 @@
 
 using namespace std;
 
-void init()
-{
-	glewInit();
+void init() {
+    glewInit();
 }
 
-void display()
-{
-	glClearColor(0.17f, 0.4f, 0.6f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void display() {
+    glClearColor(0.17f, 0.4f, 0.6f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-
-	glFinish();
-	glutSwapBuffers();
+    glFinish();
+    glutSwapBuffers();
 }
 
-void keyboard(unsigned char key, int x, int y){
-  switch(key){
-  case 27:
-    exit(0);
-    break;
-  }
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    }
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("GPGPU Lab1");
 
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
 
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-  glutInitWindowSize(600, 600);
-  glutCreateWindow("GPGPU Lab1");
-	
-  glutDisplayFunc(display);
-  glutKeyboardFunc(keyboard);
+    init();
 
-  init();
- 
-  glutMainLoop();
+    glutMainLoop();
 
-  return(0);
+    return (0);
 }
